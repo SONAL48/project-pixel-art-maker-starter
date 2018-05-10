ccd8d610af324388f61742ead9b5a1b262f74247
SONAL48 <31916140+SONAL48@users.noreply.github.com>
2018-05-09 22:25:53 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,5 +6,30 @@
 function makeGrid() {
 
 // Your code goes here!
-
+  var html="";
+  for(var i = 0; i < height; i++){
+   html +="<tr>";
+   for(var j = 0; j < width; j++){
+   html +="<td></td>";
+   }
+    html+= "<tr>";
+  }
+  $("#pixel_canvas").html(html);
 }
+$(function(){
+  var height,width;
+  $("#sizePicker").on('submit',function(event){
+    height=$("#input_height").val();
+    width=$("#input_width").val();
+    event.preventDefault();
+    makeGrid(height,width);
+  });
+  $("#pixel_canvas").on('click','td',function(){
+    var color=$("#colorPicker").val();
+    if($(this).css("background-color") == "#FFFFFF" ||$(this).css("background-color") == "rgba(0,0,0,0) || $(this).css("background-color") == "rgb(255,255,255)"){
+    $(this).css("background-color", color);
+  } else {
+      $(this).css("background-color", #FFFFFF);                   
+                  }
+
+})
